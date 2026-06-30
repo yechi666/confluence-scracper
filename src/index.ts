630ea0ba0713cd5db7c77e483468776229f5a8a0
Yechi <yechi666@gmail.com>
2026-06-30 12:24:08 +0300

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import router from './api/router';
+import { startScheduler } from './scraper/scheduler';
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 app.use(express.json());
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  startScheduler();
 });
